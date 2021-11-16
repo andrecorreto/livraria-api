b36318a0fad2219139ccff9cf79b32e875fa73cd
André Correto <andrecorreto@msn.com>
2021-11-16 15:35:41 -0300

    Inclui usuario e perfil e seguranca do spring

--- src/main/java/br/com/alura/livraria/modelo/Autor.java
+++ src/main/java/br/com/alura/livraria/modelo/Autor.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import br.com.alura.livraria.dto.AutorDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
---
+++ src/main/java/br/com/alura/livraria/service/UsuarioService.java
@@ -0,0 +1,51 @@
+package br.com.alura.livraria.service;
+
+import java.util.Random;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.alura.livraria.dto.UsuarioDto;
+import br.com.alura.livraria.dto.UsuarioFormDto;
+import br.com.alura.livraria.modelo.Perfil;
+import br.com.alura.livraria.modelo.Usuario;
+import br.com.alura.livraria.repository.PerfilRepository;
+import br.com.alura.livraria.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;	
+	@Autowired
+	private ModelMapper modelMapper;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private PerfilRepository perfilRepository;
+	
+	public Page<UsuarioDto> listar(Pageable paginacao) {
+		Page<Usuario> usuarios = usuarioRepository.findAll(paginacao);
+		return usuarios.map(u -> modelMapper.map(u, UsuarioDto.class));	
+	}
+	
+	@Transactional
+	public UsuarioDto cadastrar(UsuarioFormDto dto) {
+		Usuario usuario = modelMapper.map(dto, Usuario.class);
+		
+		Perfil perfil = perfilRepository.getById(dto.getPerfilId());
+		usuario.adicionarPerfil(perfil);
+		
+		String senha = new Random().nextInt(999999) + "";
+		System.out.println(usuario + "senha = " + senha );
+		usuario.setSenha(bCryptPasswordEncoder.encode(senha));
+		
+		usuarioRepository.save(usuario);
+		return modelMapper.map(usuario,  UsuarioDto.class);
+	}
+}
