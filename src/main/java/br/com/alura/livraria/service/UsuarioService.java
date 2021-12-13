ee861eb1af11c48bc2d31f2a8a16cb6b92158a2b
André Correto <andrecorreto@msn.com>
2021-12-13 11:23:29 -0300

    Implementa envio de email no cadastro de usuarios

--- src/main/java/br/com/alura/livraria/service/UsuarioService.java
+++ src/main/java/br/com/alura/livraria/service/UsuarioService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.alura.livraria.dto.UsuarioDto;
 import br.com.alura.livraria.dto.UsuarioFormDto;
+import br.com.alura.livraria.infra.EnviadorDeEmail;
 import br.com.alura.livraria.modelo.Perfil;
 import br.com.alura.livraria.modelo.Usuario;
 import br.com.alura.livraria.repository.PerfilRepository;
@@ -28,6 +29,8 @@ public class UsuarioService {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private PerfilRepository perfilRepository;
+	@Autowired
+	private EnviadorDeEmail enviadorDeEmail;
 	
 	public Page<UsuarioDto> listar(Pageable paginacao) {
 		Page<Usuario> usuarios = usuarioRepository.findAll(paginacao);
@@ -48,6 +51,17 @@ public class UsuarioService {
 		usuario.setSenha(bCryptPasswordEncoder.encode(senha));
 				
 		usuarioRepository.save(usuario);
+		
+		String destinatario = usuario.getEmail();
+		String assunto = "Livraria - Bem vindo(a)";
+		String mensagem = String.format("Olá %s!\n\n"
+				+ "Seguem seus dados de acesso ao sistema Livraria:\n"
+				+ "Login:%s\n"
+				+ "Senha:%s", usuario.getNome(), usuario.getLogin(), senha);
+		
+		enviadorDeEmail.enviarEmail(destinatario, assunto, mensagem);
+		
+		
 		return modelMapper.map(usuario,  UsuarioDto.class);
 	}
 }
